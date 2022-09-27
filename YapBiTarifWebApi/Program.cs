b57fa2c8f920e25415933f49f2ea6a5bec59f9ca
Ömer Kutay Yıldırım <kutay@gelal.com>
2022-09-27 14:42:06 +0300

    changed swagger

--- YapBiTarifWebApi/Program.cs
+++ YapBiTarifWebApi/Program.cs
@@ -16,12 +16,15 @@ builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 
