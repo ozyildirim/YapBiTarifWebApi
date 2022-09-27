4cddda67c9e7e89f05f12de67953e87d52c5e8aa
Ömer Kutay Yıldırım <kutay@gelal.com>
2022-09-27 15:18:57 +0300

    modified program.cs

--- YapBiTarifWebApi/Program.cs
+++ YapBiTarifWebApi/Program.cs
@@ -12,7 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>();
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
+);
 
 var app = builder.Build();
 
